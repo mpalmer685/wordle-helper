b5146d34aa2568f5f013dfcfae9232e1b11305cb
Mike Palmer <mrpalmer685@gmail.com>
2022-02-19 15:00:41 -0500

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
